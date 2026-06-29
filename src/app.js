2799a3890482b65b003f35872985455a88f8c4c2
Ramya Vutukuri <v.v.ramya1998@gmail.com>
2026-06-30 01:42:03 +0530

    exploring http methods

--- src/app.js
+++ src/app.js
@@ -2,12 +2,46 @@ console.log("Welcome Back!!....let's create a server");
 // install express to listen to our request
 const express = require("express");
 const app = express();
-app.use("/test", (req, res) => {
-  res.send("I am returning response from server");
+//order matters
+// app.use("/test/2", (req, res) => {
+//   res.send("I am returning response from server 2");
+// });
+
+// app.use("/test", (req, res) => {
+//   res.send("I am returning response from server");
+// });
+
+// app.use("/path", (req, res) => {
+//   res.send("I am in route path");
+// });
+// app.use("/", (req, res) => {
+//   res.send("I am at wild card");
+// });
+
+//use will act for all the http methods, matches path and sub paths
+app.use("/users", (req, res) => {
+  res.send("hahahahahah");
 });
-app.use("/path", (req, res) => {
-  res.send("I am in route path");
+//creating get call
+app.get("/users", (req, res) => {
+  res.send({
+    FirstName: "Ramya",
+    LastName: "Vutukuri",
+  });
 });
+
+app.post("/users", (req, res) => {
+  res.send("Data successfully saved into database");
+});
+
+app.patch("/users", (req, res) => {
+  res.send("Patched Up");
+});
+
+app.delete("/users", (req, res) => {
+  res.send("data got deleted from the database");
+});
+
 app.listen(7000, () => {
   console.log(" I am from server");
 });
