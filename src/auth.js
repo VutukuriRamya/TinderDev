4b9fc167aaa2afd1bff8dcb8b5a8c20142191f51
Ramya Vutukuri <v.v.ramya1998@gmail.com>
2026-07-23 01:07:00 +0530

    routes

--- src/auth.js
+++ src/auth.js
@@ -1,8 +1,19 @@
 const { userAuth } = require("./middleware/userAuth");
 const express = require("express");
 const app = express();
-app.use("/user/adduser", userAuth, (req, res) => {
-  console.log("validate");
+app.use("/user/adduser", (req, res) => {
+  // console.log("validate");
+  // try {
+  throw new Error("iNVALIDA AUTH");
+  // } catch {
+  //   res.send("Invalid Auth from try catch");
+  // }
+});
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    console.log(err);
+    res.send("Invalid data");
+  }
 });
 app.listen(7000, () => {
   console.log("server running");
